56d06417349128be95356c22f0cb3f3e710134bd
Construction <m.z@uchicago.edu>
2014-11-13 06:46:32 +0000

    Alberto fixed problem with displaying uploaded files by setting correct directory

--- pdfproject/__init__.py
+++ pdfproject/__init__.py
@@ -1,8 +1,50 @@
-import processing
+#import processing
+import os
+from flask import Flask, Response, request, session, g, redirect, url_for, abort, render_template, flash, send_from_directory,jsonify
+from werkzeug import secure_filename
+import shelve
+import socket
 
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+UPLOAD_FOLDER = './pdfproject/static/uploads'
+ALLOWED_EXTENSIONS = set(['pdf','PDF'])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route("/")
 def hello():
 	return "Hello World!"
+
+@app.route("/start/")
+def homeprint():
+	return render_template("home.html")
+
+@app.route("/process/",methods=['GET','POST'])
+def upload_file():
+	print 'upload file'
+	try:
+		os.stat(app.config['UPLOAD_FOLDER'])
+	except:
+		os.mkdir(app.config['UPLOAD_FOLDER'])
+	if request.method == 'POST':
+		file = request.files['uploadFile']
+		print type(file.stream)
+		print 'filename' + file.filename
+		if file and allowed_file(file.filename):
+			print 'allowing file'
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			print "saved?"
+			return "upload success" #redirect(url_for('uploaded_file',filename=filename))
+		return "No!"
+	return "NO"	
+
+'''
+@app.route('/static/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+'''
