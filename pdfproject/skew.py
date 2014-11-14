f763a7ecb68f2a73a154584a306a7105e8c6cad8
Catherine Moresco <catherine.moresco@gmail.com>
2014-11-14 01:48:25 -0600

    fix skew output

--- pdfproject/skew.py
+++ pdfproject/skew.py
@@ -16,7 +16,8 @@ def horizontal_sums(img):
 	return sums
 
 # rotate an image by an angle that maximizes the standard deviation of its row sums
-def straighten(img):
+def straighten(input_img):
+	img = np.copy(input_img)
 	if len(img.shape) > 2:
 		img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
@@ -34,4 +35,4 @@ def straighten(img):
 		stds[np.std(horizontal_sums(rotate(img, i)))] = i
 	angle = stds[max(stds.keys())]
 	
-	return rotate(img, angle)
+	return rotate(input_img, angle)
