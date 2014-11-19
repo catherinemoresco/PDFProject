9dba0ff2e02a88da5307f96db8f2d35fc989c92d
Jonathan Jin <jjin082693@gmail.com>
2014-11-19 16:50:16 -0600

    finish refactor ui tests

--- tests/uitests/ui_tests.js
+++ tests/uitests/ui_tests.js
@@ -38,92 +38,31 @@ m += testOutput(9, setHRectangle(ll[0]-10,l[1]-4,1[2]+10,l[3]-4)
 
 //Test 10: Proper Highlighting
 var j = getLine(3);
-if(setHRectangle(j[0],j[1],j[2],j[3]) == true){
-  if(compareHRectangle(l[0],l[1],1[2],l[3]) == false){
-    m = m + " Test 9: passed\n";
-  }
-  else{
-    m = m + " Test 9: Failed\n";
-  }
-}
-else{
-  m = m + " Test 9: Failed\n";
-}
+m += testOutput(10, setHRectangle(j[0],j[1],j[2],j[3])
+    && !compareHRectangle(l[0],l[1],1[2],l[3]));
 //There are other cases in this category I'd like to implement but these require already having analyzed a pdf file
 
 //Test 11: Proper annotating 
-if(setARectangle(-100,-100,0,0,"lkdvnjanvlfanlfna") == false){
-  m = m + " Test 11: Passed\n";
-}
-else{
-  m = m + " Test 11: Failed\n";
-}
+m += testOutput(11, !setARectangle(-100,-100,0,0,"lkdvnjanvlfanlfna"));
 
 //Test 12: Proper annotating 
-if(setARectangle(6000,6000,7000,7000,"assdvafb"); == false){
-  m = m + " Test 12: Passed\n";
-}
-else{
-  m = m + " Test 12: Failed\n";
-}
+m += testOutput(12, !setARectangle(6000,6000,7000,7000,"assdvafb"));
 
 //Test 13: Proper annotating 
-if(setARectangle(0,0,0,0,"snddddddddddddddddddddddn") == false){
-  m = m + " Test 13: Passed\n";
-}
-else{
-  m = m + " Test 13: Failed\n";
-}
+m += testOutput(13, !setARectangle(0,0,0,0,"snddddddddddddddddddddddn"));
 
 //Test 14: Proper annotating 
-if(setARectangle(50,50,100,100,"") == true){
-  m = m + " Test 14: Passed\n";
-}
-else{
-  m = m + " Test 14: Failed\n";
-}
-if(setARectangle(50,50,100,100,"hello world") == true){
-  m = m + " Test 13: Passed\n";
-}
-else{
-  m = m + " Test 13: Failed\n";
-}
+m += testOutput(14, setARectangle(50,50,100,100,""));
+m += testOutput(13, setARectangle(50,50,100,100,"hello world")); // FIXME
 
 //Test 15: Proper annotating 
-if(setARectangle(300,50,400,100,"HELLO") == true){
-  if(compareARectangle(300,50,400,100,"HELLO") == true){
-    m = m + " Test 14: passed\n";
-  }
-  else{
-    m = m + " Test 14: Failed\n";
-  }
-}
-else{
-  m = m + " Test 14: Failed\n";
-}
+m += testOutput(15, setARectangle(300,50,400,100,"HELLO")
+    && compareARectangle(300,50,400,100,"HELLO"));
 
 //Test 16: Proper annotating 
-if(setARectangle(50,50,100,100,"HELLO") == true){
-  if(compareARectangle(50,50,100,100,"HELLO") == true){
-    m = m + " Test 16: passed\n";
-  }
-  else{
-    m = m + " Test 16: Failed\n";
-  }
-}
-else{
-  m = m + " Test 14: Failed\n";
-}
+m += testOutput(16, setARectangle(50,50,100,100,"HELLO")
+    && compareARectangle(50,50,100,100,"HELLO"));
 
 //Test 17: Proper annotating 
-if(setARectangle(50,50,100,100,"HELLO") == true){
-  if(compareARectangle(50,50,100,100,"") == false){
-    m = m + " Test 16: passed\n";
-  }
-  else{
-    m = m + " Test 16: Failed\n";
-  }
-}
-else{
-  m = m + " Test 14: Failed\n";
-}
+m += testOutput(17, setARectangle(50,50,100,100,"HELLO") 
+    && !compareARectangle(50,50,100,100,""));
