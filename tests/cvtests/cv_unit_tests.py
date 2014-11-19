1470e9110133a80bd27749ea7e75cc81f84e70f4
Megan Barnes <meganbarnes@wireless-s1-no-150-22-97.uchicago.edu>
2014-11-18 19:22:00 -0600

    New unit tests for iteration 2

--- tests/cvtests/cv_unit_tests.py
+++ tests/cvtests/cv_unit_tests.py
@@ -204,6 +204,77 @@ class skewed_text_test_case(unittest.TestCase):
     	#compares calculated angle to observed angle to make sure they are equal
     	print self.angle
     	self.assertTrue(abs(self.angle + 15) < 2)
+ 
+## added for iteration 2   	
+
+class noisy_text1(unittest.TestCase):
+
+    def setUp(self):
+    	#one picture image
+        self.image = cv2.imread("testimg/noisy1.jpg")
+        self.img, self.angle = skew.straighten(cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY))
+        self.lines = json.loads(processing.getLines(self.img)[1])
+        
+    def test_merge_text(self):
+    	#makes sure number of contours is decreasing through processing
+    	after_len = len(cv2.findContours(processing.isolateLines(self.img), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0])
+    	before_len = len(cv2.findContours(cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0])
+    	self.assertTrue(after_len <= before_len)
+
+    def test_text_area_dilation(self):
+     	#compares number of detected lines to number we observe to make sure they are equal
+        self.assertTrue((len(self.lines) - 20) < 5 and (len(self.lines) - 20) >= 0)
+    
+    def test_calculate_angle(self):
+    	#compares calculated angle to observed angle to make sure they are equal
+    	print self.angle
+    	self.assertTrue(abs(self.angle - 4) < 2)
+    	
+class noisy_text2(unittest.TestCase):
+
+    def setUp(self):
+    	#one picture image
+        self.image = cv2.imread("testimg/noisy2.jpg")
+        self.img, self.angle = skew.straighten(cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY))
+        self.lines = json.loads(processing.getLines(self.img)[1])
+        
+    def test_merge_text(self):
+    	#makes sure number of contours is decreasing through processing
+    	after_len = len(cv2.findContours(processing.isolateLines(self.img), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0])
+    	before_len = len(cv2.findContours(cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0])
+    	self.assertTrue(after_len <= before_len)
+
+    def test_text_area_dilation(self):
+     	#compares number of detected lines to number we observe to make sure they are equal
+        self.assertTrue((len(self.lines) - 44) < 5 and (len(self.lines) - 44) >= 0)
+    
+    def test_calculate_angle(self):
+    	#compares calculated angle to observed angle to make sure they are equal
+    	print self.angle
+    	self.assertTrue(abs(self.angle + 2) < 2)
+    	
+class black_margins(unittest.TestCase):
+
+    def setUp(self):
+    	#one picture image
+        self.image = cv2.imread("testimg/blackmargins.jpg")
+        self.img, self.angle = skew.straighten(cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY))
+        self.lines = json.loads(processing.getLines(self.img)[1])
+        
+    def test_merge_text(self):
+    	#makes sure number of contours is decreasing through processing
+    	after_len = len(cv2.findContours(processing.isolateLines(self.img), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0])
+    	before_len = len(cv2.findContours(cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0])
+    	self.assertTrue(after_len <= before_len)
+
+    def test_text_area_dilation(self):
+     	#compares number of detected lines to number we observe to make sure they are equal
+        self.assertTrue((len(self.lines) - 46) < 5 and (len(self.lines) - 46) >= 0)
+    
+    def test_calculate_angle(self):
+    	#compares calculated angle to observed angle to make sure they are equal
+    	print self.angle
+    	self.assertTrue(abs(self.angle - 2) < 2)
 
 	
 if __name__ == '__main__':
