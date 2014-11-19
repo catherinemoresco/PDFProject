f0442058131fee2a82ae67d39de4c3a92e6465d3
Jonathan Jin <jjin082693@gmail.com>
2014-11-19 17:33:12 -0600

    rm redundant test suite comments

--- tests/uitests/ui_tests.js
+++ tests/uitests/ui_tests.js
@@ -44,42 +44,34 @@ test(9, setHRectangle(l[0]-10,l[1]-4,1[2]+10,l[3]-4)
     && compareHRectangle(l[0],l[1],1[2],l[3]), true,
      "setHRectangle works"); // FIXME: descriptor
 
-//Test 10: Proper Highlighting
 j = getLine(3);
 test(10, setHRectangle(j[0],j[1],j[2],j[3])
     && !compareHRectangle(l[0],l[1],1[2],l[3]), true,
      "setHRectangle works"); // FIXME: descriptor
 //There are other cases in this category I'd like to implement but these require already having analyzed a pdf file
 
-//Test 11: Proper annotating 
 test(11, setARectangle(-100,-100,0,0,"lkdvnjanvlfanlfna"), false,
     "setARectangle does not take negative dimensions");
 
-//Test 12: Proper annotating 
 test(12, setARectangle(6000,6000,7000,7000,"assdvafb"), false,
     "setARectangle does not take abnormally large dimensions");
 
-//Test 13: Proper annotating 
 test(13, setARectangle(0,0,0,0,"snddddddddddddddddddddddn"), false,
     "setARectangle does not take value 0 dimensions");
 
-//Test 14: Proper annotating 
 test(14, setARectangle(50,50,100,100,""), true,
     "setARectangle() accepts the empty string");
 test(13, setARectangle(50, 50, 100, 100, "hello world"), true,
     "setARectangle() accepts general string input"); // FIXME
 
-//Test 15: Proper annotating 
 test(15, setARectangle(300,50,400,100,"HELLO")
     && compareARectangle(300,50,400,100,"HELLO"), true,
     "setARectangle() works"); // FIXME: descriptor
 
-//Test 16: Proper annotating 
 test(16, setARectangle(50,50,100,100,"HELLO")
     && compareARectangle(50,50,100,100,"HELLO"), true,
     "setARectangle() works"); // FIXME: descriptor
 
-//Test 17: Proper annotating 
 test(17, setARectangle(50,50,100,100,"HELLO") 
     && !compareARectangle(50,50,100,100,""), true,
     "setARectangle() works"); // FIXME: descriptor
