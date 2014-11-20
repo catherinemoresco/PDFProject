f52d33c7f72b7720abc32371bd75175b9ebe4698
Jonathan Jin <jjin082693@gmail.com>
2014-11-19 22:01:56 -0600

    use unittest for highlight tests

--- tests/uitests/highlight_tests.py
+++ tests/uitests/highlight_tests.py
@@ -1,5 +1,14 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome()
-driver.close()
+class Highlight(unittest.TestCase)
+    def setUp(self);
+        driver = webdriver.Chrome()
+
+    def tearDown(self);
+        driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
