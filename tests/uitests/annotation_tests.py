1961c3a2134b6eefe3d390f8b385dbeb0b4f35dc
saucedoc <saucedoc@uchicago.edu>
2014-11-20 05:16:48 -0600

    ui unit test

--- tests/uitests/annotation_tests.py
+++ tests/uitests/annotation_tests.py
@@ -5,21 +5,136 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class Annotate(unittest.TestCase):
+#test  5 negative off page text box
+class Annotate_Neg_Page(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
-
-        # FIXME
-        response = self.driver.get("http://localhost:5000")
+        filePath = os.getcwd() +'test.pdf'
+        driver.get("http://spark.uchicago.edu/start/")
+        uploadF = driver.getelement_by_name('fileUpload')
+        uploadF.click()
+        uploadF.send_keys(filePath)
+        elem.send_keys(Keys.RETURN)
+        driver.getelement_by_name('Submit').click()  
+        #response = self.driver.get("http://localhost:5000")
 
     def test_annotation_creation(self):
-        # TODO:
-        # - click
-        # - assert created new annotation
-        return
+        actions = ActionChains(driver)
+        actions.click(button)
+        actions.move_to_element_with_offset(html, -100, -100)
+        actions.click()
+        actions.send_keys("test text box")
+        actions.send_keys(Keys.RETURN)
+        actions.perform()
+        self.assertRaises(NoSuchElementException, actions.find_element_by_id, "textbox") 
 
     def tearDown(self):
         self.driver.close()
+
+#test  6 large off page text box
+class Annotate_Large_Page(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        filePath = os.getcwd() +'test.pdf'
+        driver.get("http://spark.uchicago.edu/start/")
+        uploadF = driver.getelement_by_name('fileUpload')
+        uploadF.click()
+        uploadF.send_keys(filePath)
+        elem.send_keys(Keys.RETURN)
+        driver.getelement_by_name('Submit').click()  
+        #response = self.driver.get("http://localhost:5000")
+
+    def test_annotation_creation(self):
+        actions = ActionChains(driver)
+        actions.click(button)
+        actions.move_to_element_with_offset(html, 6000, 6000)
+        actions.click()
+        actions.send_keys("test text box")
+        actions.send_keys(Keys.RETURN)
+        actions.perform()
+        self.assertRaises(NoSuchElementException, actions.find_element_by_id, "textbox") 
+
+    def tearDown(self):
+        self.driver.close()
+
+#test  7 test empty text box
+class Annotate_Empty(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        filePath = os.getcwd() +'test.pdf'
+        driver.get("http://spark.uchicago.edu/start/")
+        uploadF = driver.getelement_by_name('fileUpload')
+        uploadF.click()
+        uploadF.send_keys(filePath)
+        elem.send_keys(Keys.RETURN)
+        driver.getelement_by_name('Submit').click()  
+        #response = self.driver.get("http://localhost:5000")
+
+    def test_annotation_creation(self):
+        actions = ActionChains(driver)
+        actions.click(button)
+        actions.move_to_element_with_offset(html, 50, 50)
+        actions.click()
+        actions.send_keys(Keys.RETURN)
+        actions.perform()
+        self.assertRaises(NoSuchElementException, actions.find_element_by_id, "textbox") 
+
+    def tearDown(self):
+        self.driver.close()
+
+#test  8 test regular text box
+class Annotate_TextBox_Creation(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        filePath = os.getcwd() +'test.pdf'
+        driver.get("http://spark.uchicago.edu/start/")
+        uploadF = driver.getelement_by_name('fileUpload')
+        uploadF.click()
+        uploadF.send_keys(filePath)
+        elem.send_keys(Keys.RETURN)
+        driver.getelement_by_name('Submit').click()  
+        #response = self.driver.get("http://localhost:5000")
+
+    def test_annotation_creation(self):
+        actions = ActionChains(driver)
+        actions.click(button)
+        actions.move_to_element_with_offset(html, 50, 50)
+        actions.click()
+        actions.send_keys("test text box")
+        actions.send_keys(Keys.RETURN)
+        actions.perform()
+        self.assert(actions.find_element_by_id("textbox"))
+
+    def tearDown(self):
+        self.driver.close()
+
+#test  9 check text box text
+class Annotate_Correct_Text(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        filePath = os.getcwd() +'test.pdf'
+        driver.get("http://spark.uchicago.edu/start/")
+        uploadF = driver.getelement_by_name('fileUpload')
+        uploadF.click()
+        uploadF.send_keys(filePath)
+        elem.send_keys(Keys.RETURN)
+        driver.getelement_by_name('Submit').click()  
+        #response = self.driver.get("http://localhost:5000")
+
+    def test_annotation_creation(self):
+        actions = ActionChains(driver)
+        actions.click(button)
+        actions.move_to_element_with_offset(html, 300, 50)
+        actions.click()
+        actions.send_keys("test text box")
+        actions.send_keys(Keys.RETURN)
+        actions.perform()
+        self.assertEqual(actions.find_element_by_id("textbox").get_attribute('value'), "test text box")
+
+    def tearDown(self):
+        self.driver.close()
+
+
 
 if __name__=="__main":
     unittest.main()
--- tests/uitests/highlight_tests.py
+++ tests/uitests/highlight_tests.py
@@ -5,20 +5,111 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class Highlight(unittest.TestCase):
+#test  1 negative off page highlight
+class Highlight_Neg_Page(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
-
-        # FIXME: I am so sorry
-        response = self.driver.get("http://localhost:5000")
+        filePath = os.getcwd() +'test.pdf'
+        driver.get("http://spark.uchicago.edu/start/")
+        uploadF = driver.getelement_by_name('fileUpload')
+        uploadF.click()
+        uploadF.send_keys(filePath)
+        elem.send_keys(Keys.RETURN)
+        driver.getelement_by_name('Submit').click()
+        #response = self.driver.get("http://localhost:5000")
 
 
     def test_highlight_creation(self):
-        # TODO:
-        # - click and drag
-        # - assert created new highlight
+        actions = ActionChains(driver)
+        actions.move_to_element_with_offset(html, -100, -100)
+        actions.click_and_hold()
+        actions.move_to_element_with_offset(html, 0, 0)
+        actions.release()
+        actions.perform()
+        self.assertRaises(NoSuchElementException, actions.find_element_by_id, "highlight") 
 
-        return
+        
+
+    def tearDown(self):
+        self.driver.close()
+
+#test  2 large off page highlight
+class Highlight_Large_Page(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        filePath = os.getcwd() +'test.pdf'
+        driver.get("http://spark.uchicago.edu/start/")
+        uploadF = driver.getelement_by_name('fileUpload')
+        uploadF.click()
+        uploadF.send_keys(filePath)
+        elem.send_keys(Keys.RETURN)
+        driver.getelement_by_name('Submit').click()
+        #response = self.driver.get("http://localhost:5000")
+
+
+    def test_highlight_creation(self):
+        actions = ActionChains(driver)
+        actions.move_to_element_with_offset(html, 6000, 6000)
+        actions.click_and_hold()
+        actions.move_to_element_with_offset(html, 7000, 7000)
+        actions.release()
+        actions.perform()
+        self.assertRaises(NoSuchElementException, actions.find_element_by_id, "highlight") 
+        
+
+    def tearDown(self):
+        self.driver.close()
+
+#test  3 no movement highlight
+class Highlight_No_Move(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        filePath = os.getcwd() +'test.pdf'
+        driver.get("http://spark.uchicago.edu/start/")
+        uploadF = driver.getelement_by_name('fileUpload')
+        uploadF.click()
+        uploadF.send_keys(filePath)
+        elem.send_keys(Keys.RETURN)
+        driver.getelement_by_name('Submit').click()
+        #response = self.driver.get("http://localhost:5000")
+
+
+    def test_highlight_creation(self):
+        actions = ActionChains(driver)
+        actions.move_to_element_with_offset(html, 0, 0)
+        actions.click_and_hold()
+        actions.move_to_element_with_offset(html, 0, 0)
+        actions.release()
+        actions.perform()
+        self.assertRaises(NoSuchElementException, actions.find_element_by_id, "highlight") 
+
+        
+    def tearDown(self):
+        self.driver.close()
+
+#test  4 working highlight
+class Highlight_Good_Highlight(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        filePath = os.getcwd() +'test.pdf'
+        driver.get("http://spark.uchicago.edu/start/")
+        uploadF = driver.getelement_by_name('fileUpload')
+        uploadF.click()
+        uploadF.send_keys(filePath)
+        elem.send_keys(Keys.RETURN)
+        driver.getelement_by_name('Submit').click()
+        #response = self.driver.get("http://localhost:5000")
+
+
+    def test_highlight_creation(self):
+        actions = ActionChains(driver)
+        actions.move_to_element_with_offset(html, 300, 300)
+        actions.click_and_hold()
+        actions.move_to_element_with_offset(html, 400, 400)
+        actions.release()
+        actions.perform()
+        self.assert(actions.find_element_by_id("highlight"))
+
 
     def tearDown(self):
         self.driver.close()
