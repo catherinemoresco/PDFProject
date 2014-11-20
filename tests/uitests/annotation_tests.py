5ab452c9fc88927dd1d638c97d7e877602ddd9ad
Jonathan Jin <jjin082693@gmail.com>
2014-11-19 22:20:06 -0600

    set up front-end testing framework

--- tests/uitests/annotation_tests.py
+++ tests/uitests/annotation_tests.py
@@ -1,5 +1,20 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome()
-driver.close()
+class Annotate(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def test_annotation_creation(self):
+        # TODO:
+        # - click
+        # - assert created new annotation
+
+        return
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__=="__main":
+    unittest.main()
--- tests/uitests/highlight_tests.py
+++ tests/uitests/highlight_tests.py
@@ -2,13 +2,23 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class Highlight(unittest.TestCase)
-    def setUp(self);
-        driver = webdriver.Chrome()
+from flask import url_for
 
-    def tearDown(self);
-        driver.close()
 
+class Highlight(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+
+    def test_highlight_creation(self):
+        # TODO:
+        # - click and drag
+        # - assert created new highlight
+
+        return
+
+    def tearDown(self):
+        self.driver.close()
 
 if __name__ == "__main__":
     unittest.main()
