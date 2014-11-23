b6c27499b7b464ea127c3b844736c307a61bb376
Jonathan Jin <jjin082693@gmail.com>
2014-11-22 21:11:30 -0600

    refactor and modularize uitest suite

--- tests/uitests/highlight_tests.py
+++ tests/uitests/highlight_tests.py
@@ -5,8 +5,7 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-#test  1 negative off page highlight
-class Highlight_Neg_Page(unittest.TestCase):
+class Highlight(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
         filePath = os.getcwd() +'test.pdf'
@@ -19,7 +18,7 @@ class Highlight_Neg_Page(unittest.TestCase):
         #response = self.driver.get("http://localhost:5000")
 
 
-    def test_highlight_creation(self):
+    def test_negative_off_page(self):
         actions = ActionChains(driver)
         actions.move_to_element_with_offset(html, -100, -100)
         actions.click_and_hold()
@@ -28,26 +27,7 @@ class Highlight_Neg_Page(unittest.TestCase):
         actions.perform()
         self.assertRaises(NoSuchElementException, actions.find_element_by_id, "highlight") 
 
-        
-
-    def tearDown(self):
-        self.driver.close()
-
-#test  2 large off page highlight
-class Highlight_Large_Page(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        filePath = os.getcwd() +'test.pdf'
-        driver.get("http://spark.uchicago.edu/start/")
-        uploadF = driver.getelement_by_name('fileUpload')
-        uploadF.click()
-        uploadF.send_keys(filePath)
-        elem.send_keys(Keys.RETURN)
-        driver.getelement_by_name('Submit').click()
-        #response = self.driver.get("http://localhost:5000")
-
-
-    def test_highlight_creation(self):
+    def test_large_off_page(self):
         actions = ActionChains(driver)
         actions.move_to_element_with_offset(html, 6000, 6000)
         actions.click_and_hold()
@@ -55,26 +35,8 @@ class Highlight_Large_Page(unittest.TestCase):
         actions.release()
         actions.perform()
         self.assertRaises(NoSuchElementException, actions.find_element_by_id, "highlight") 
-        
 
-    def tearDown(self):
-        self.driver.close()
-
-#test  3 no movement highlight
-class Highlight_No_Move(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        filePath = os.getcwd() +'test.pdf'
-        driver.get("http://spark.uchicago.edu/start/")
-        uploadF = driver.getelement_by_name('fileUpload')
-        uploadF.click()
-        uploadF.send_keys(filePath)
-        elem.send_keys(Keys.RETURN)
-        driver.getelement_by_name('Submit').click()
-        #response = self.driver.get("http://localhost:5000")
-
-
-    def test_highlight_creation(self):
+    def test_zero_size(self):
         actions = ActionChains(driver)
         actions.move_to_element_with_offset(html, 0, 0)
         actions.click_and_hold()
@@ -83,25 +45,7 @@ class Highlight_No_Move(unittest.TestCase):
         actions.perform()
         self.assertRaises(NoSuchElementException, actions.find_element_by_id, "highlight") 
 
-        
-    def tearDown(self):
-        self.driver.close()
-
-#test  4 working highlight
-class Highlight_Good_Highlight(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        filePath = os.getcwd() +'test.pdf'
-        driver.get("http://spark.uchicago.edu/start/")
-        uploadF = driver.getelement_by_name('fileUpload')
-        uploadF.click()
-        uploadF.send_keys(filePath)
-        elem.send_keys(Keys.RETURN)
-        driver.getelement_by_name('Submit').click()
-        #response = self.driver.get("http://localhost:5000")
-
-
-    def test_highlight_creation(self):
+    def test_valid(self):
         actions = ActionChains(driver)
         actions.move_to_element_with_offset(html, 300, 300)
         actions.click_and_hold()
@@ -110,9 +54,5 @@ class Highlight_Good_Highlight(unittest.TestCase):
         actions.perform()
         self.assert(actions.find_element_by_id("highlight"))
 
-
     def tearDown(self):
         self.driver.close()
-
-if __name__ == "__main__":
-    unittest.main()
--- tests/uitests/uploadTest.py
+++ tests/uitests/uploadTest.py
@@ -2,42 +2,43 @@ import unittest
 import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-class Highlight(unittest.TestCase)
+
+filePath = os.getcwd() +'test.pdf'
+
+class Upload(unittest.TestCase)
     def setUp(self):
         driver = webdriver.Chrome()
-	def test_upload(self)
-		filePath = os.getcwd() +'test.pdf'
-		
-		#Correct File Upload
-		driver.get("http://spark.uchicago.edu/start/")
-		uploadF = driver.getelement_by_name('fileUpload')
-		uploadF.click()
-		uploadF.send_keys(filePath)
-		elem.send_keys(Keys.RETURN)
-		driver.getelement_by_name('Submit').click()
-		assert "File Upload Success" in driver.page_source
-		
-		#Blank name entered
-		driver.get("http://spark.uchicago.edu/start/")
-		uploadF = driver.getelement_by_name('fileUpload')
-		uploadF.click()
-		uploadF.send_keys("")
-		elem.send_keys(Keys.RETURN)
-		driver.getelement_by_name('Submit').click()
-		assert "File Upload Success" not in driver.page_source
 
-		#Not a PDF uploaded
-		filePath = os.getcwd() +'test.jpg'
-		driver.get("http://spark.uchicago.edu/start/")
-		uploadF = driver.getelement_by_name('fileUpload')
-		uploadF.click()
-		uploadF.send_keys("")
-		elem.send_keys(Keys.RETURN)
-		driver.getelement_by_name('Submit').click()
-		assert "File Upload Success" not in driver.page_source		
-		
-	
-	def tearDown(self);
+    def test_valid(self):
+        driver.get("http://spark.uchicago.edu/start/")
+        uploadF = driver.getelement_by_name('fileUpload')
+        uploadF.click()
+        uploadF.send_keys(filePath)
+        elem.send_keys(Keys.RETURN)
+        driver.getelement_by_name('Submit').click()
+        assert "File Upload Success" in driver.page_source
+
+    def test_blank_name(self):
+        driver.get("http://spark.uchicago.edu/start/")
+        uploadF = driver.getelement_by_name('fileUpload')
+        uploadF.click()
+        uploadF.send_keys("")
+        elem.send_keys(Keys.RETURN)
+        driver.getelement_by_name('Submit').click()
+        assert "File Upload Success" not in driver.page_source
+
+    def test_invalid_file_format(self):
+        filePath = os.getcwd() +'test.jpg'
+        driver.get("http://spark.uchicago.edu/start/")
+        uploadF = driver.getelement_by_name('fileUpload')
+        uploadF.click()
+        uploadF.send_keys("")
+        elem.send_keys(Keys.RETURN)
+        driver.getelement_by_name('Submit').click()
+        assert "File Upload Success" not in driver.page_source		
+
+    def tearDown(self);
         driver.close()
+
 if __name__ == "__main__":
     unittest.main()
